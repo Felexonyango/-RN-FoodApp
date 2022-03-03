3daad214b21716a7fb3452bff5aa89b7c51beefd
Felexonyango <felexonyango19@gmail.com>
2022-03-03 16:39:56 +0300

    working on firebase auth

--- App.js
+++ App.js
@@ -1,20 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import 'react-native-gesture-handler';
+
+import {Text,LogBox} from 'react-native'
+import React, { useEffect, useState } from 'react'
+import  {auth}  from './src/firebase/config'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import RegistrationScreen from "./src/screen/auth/Registration"
+import LoginScreen from "./src/screen/auth/LoginScreen"
+import HomeScreen from "./src/screen/HomeScreen"
+
+LogBox.ignoreLogs([
+  "Setting a timer",
+  "AsyncStorage has been extracted from react-native core and will be removed in a future release.",
+]);
+
+
+const StackAuth = createStackNavigator();
+const AppStack = createStackNavigator();
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [user, setUser] = useState(null)
+  const [Loading, setLoading] = useState(true);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  useEffect(() => {
+
+    const unsubscribeAuth = auth.onAuthStateChanged(async authUser => {
+      try {
+        await (authUser ? setUser(authUser) : setUser(null));
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+    
+    return unsubscribeAuth;
+  }, []);
+ 
+  // useEffect(() => {
+  //   const unsubscribe = onAuthStateChanged(auth, (user) => {
+  //     setLoading(false);
+  //     if (user) {
+  //       setCurrUser(user);
+  //     }
+  //   });
+  //   return () => unsubscribe();
+  // }, []);
+
+  if (Loading) {
+    return <Text>Loading...</Text>;
+  }
+  const AppHome = () => {
+    return (
+      <AppStack.Navigator>
+        <AppStack.Screen name='Home'component={HomeScreen}/>
+        
+      </AppStack.Navigator>
+    )
+  }
+
+  const Auth =()=>{
+    return(
+
+      <StackAuth.Navigator>
+      <StackAuth.Screen name='Login'component={LoginScreen}/>
+      <StackAuth.Screen name='Registration'component={RegistrationScreen}/>
+      </StackAuth.Navigator>
+  
+    )
+   
+
+  }
+  return (
+    
+<NavigationContainer>
+{user ? <AppHome/> : <Auth/>}
+
+</NavigationContainer>
+  )
+} 
---
+++ src/screen/auth/Registration.js
@@ -0,0 +1,104 @@
+import React, { useState } from 'react'
+import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import firebase,{auth,signUp} from '../../firebase/config';
+import styles from './style';
+
+
+export default function RegistrationScreen({navigation}) {
+    const [fullName, setFullName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+ 
+    const onFooterLinkPress = () => {
+        navigation.navigate('Login')
+    }
+    const handleSignUp = async () => {
+      
+
+        try {
+            if (password !== confirmPassword) {
+                alert("Passwords don't match.")
+                return
+            }
+            await signUp(email, password);
+        } catch (error) {
+            alert(error.message);
+        }
+        console.log(auth.currentUser.uid)
+     
+
+        firebase.firestore().collection('users').doc(auth.currentUser.uid).set({
+            name: user.name,
+            email: user.email,
+            uid: auth.currentUser.uid,
+        }).then(() => {
+            console.log('hello create user ok')
+        }).catch((err) => {
+            alert('err' + err)
+        })
+    }
+
+
+
+
+    return (
+        <View style={styles.container}>
+            <KeyboardAwareScrollView
+                style={{ flex: 1, width: '100%' }}
+                keyboardShouldPersistTaps="always">
+                <Image
+                    style={styles.logo}
+                    source={require('../../../assets/icon.png')}
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder='Full Name'
+                    placeholderTextColor="#aaaaaa"
+                    onChangeText={(text) => setFullName(text)}
+                    value={fullName}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder='E-mail'
+                    placeholderTextColor="#aaaaaa"
+                    onChangeText={(text) => setEmail(text)}
+                    value={email}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholderTextColor="#aaaaaa"
+                    secureTextEntry
+                    placeholder='Password'
+                    onChangeText={(text) => setPassword(text)}
+                    value={password}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholderTextColor="#aaaaaa"
+                    secureTextEntry
+                    placeholder='Confirm Password'
+                    onChangeText={(text) => setConfirmPassword(text)}
+                    value={confirmPassword}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                />
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => handleSignUp()}>
+                    <Text style={styles.buttonTitle}>Create account</Text>
+                </TouchableOpacity>
+                <View style={styles.footerView}>
+                    <Text style={styles.footerText}>Already got an account? <Text onPress={onFooterLinkPress} style={styles.footerLink}>Log in</Text></Text>
+                </View>
+            </KeyboardAwareScrollView>
+        </View>
+    )
+}
